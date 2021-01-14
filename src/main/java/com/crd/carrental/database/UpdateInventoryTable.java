8e62e05aeeab7f19438cb24003af35e19eb07b0b
Michael Lewis <mjolewis@gmail.com>
2021-01-14 12:40:57 -0500

    FIX: Fixed syntax error for update statement
    -There was a syntax error when updating the cars database after a reservation was made. This error caused the application to fail.

--- src/main/java/com/crd/carrental/database/UpdateInventoryTable.java
+++ src/main/java/com/crd/carrental/database/UpdateInventoryTable.java
@@ -28,10 +28,10 @@ public class UpdateInventoryTable implements UpdateStrategy {
     public void update(ReservationController controller) {
 
         String updateStatement = "UPDATE cars " +
-                "SET isReserved = ? " +
-                "SET isAvailable = ? " +
-                "SET reservationStartDateAndTime = ? " +
-                "SET reservationEndDateAndTime = ? " +
+                "SET isReserved = ?, " +
+                "isAvailable = ?, " +
+                "reservationStartDateAndTime = ?, " +
+                "reservationEndDateAndTime = ? " +
                 "WHERE vin = ?";
 
         try {
@@ -44,15 +44,16 @@ public class UpdateInventoryTable implements UpdateStrategy {
         CloseConnection.closeQuietly(pStmt);
     }
 
-    // Prepared Statements help prevent SQL injection and efficiently execute the statement
     private void createPreparedStatement(String updateStatement, ReservationController controller) throws SQLException {
 
         Timestamp convertedStartDateAndTime = convertDateAndTime(controller.getReservationStartDateAndTime());
         Timestamp convertedEndDateAndTime = convertDateAndTime(controller.getReservationStartDateAndTime());
 
+        boolean isValidReservationDateAndTime = isStartAndEndValid(convertedStartDateAndTime, convertedEndDateAndTime);
+
         pStmt = con.prepareStatement(updateStatement);
         pStmt.setBoolean(1, true);
-        pStmt.setBoolean(2, isCarAvailable());
+        pStmt.setBoolean(2, isValidReservationDateAndTime);
         pStmt.setTimestamp(3, convertedStartDateAndTime);
         pStmt.setTimestamp(4, convertedEndDateAndTime);
         pStmt.setString(5, controller.getVin());
@@ -62,13 +63,12 @@ public class UpdateInventoryTable implements UpdateStrategy {
         return Timestamp.valueOf(dateAndTime.replaceAll("T", " ") + ":00");
     }
 
-    private boolean isCarAvailable() {
+    // A valid reservation request can only happen if the date and time are in the future and if the end date and time
+    // are greater than the start date and time
+    private boolean isStartAndEndValid(Timestamp startDateAndTime, Timestamp endDateAndTime) {
         Timestamp current = Timestamp.valueOf(new Timestamp(System.currentTimeMillis()).toString());
-        //Timestamp reservationTime = Timestamp.valueOf(reservationStartDateAndTime.toString());
-        //int compare = current.compareTo(reservationTime);
 
-        //return compare <= 0;
-        return true;
+        return startDateAndTime.compareTo(endDateAndTime) <= 0 && current.compareTo(startDateAndTime) <= 0;
     }
 
     private void executeUpdate() throws SQLException {
